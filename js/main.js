9ba736a180b8fe195f0d1d94aa34699d757f1839
Bozhuk <bozhukihor1994@gmail.com>
2021-10-23 17:29:37 +0300

    finish

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+    $(".slider__box").slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="/images/Arrow-left.png" alt="">',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="/images/Arrow-right.png" alt="">'
+    });
+
+});
