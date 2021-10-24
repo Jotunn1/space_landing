e06daca4457757f58d78892eaaf04e896266766c
Bozhuk <bozhukihor1994@gmail.com>
2021-10-24 21:01:41 +0300

    Finish

--- js/main.js
+++ js/main.js
@@ -4,4 +4,7 @@ $(function () {
         nextArrow: '<img class="slider__arrow slider__arrow-right" src="/images/Arrow-right.png" alt="">'
     });
 
+    $(".menu__btn").on("click", function () {
+        $(".menu__list").toggleClass("active")
+    });
 });
